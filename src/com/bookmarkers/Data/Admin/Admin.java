e1923bf03c036d88041985dcd6e2eed8dab2d286
Yutong Jin <yjin3@scu.edu>
2018-08-16 16:06:55 -0700

    little change

--- src/com/bookmarkers/Data/Admin/Admin.java
+++ src/com/bookmarkers/Data/Admin/Admin.java
@@ -1,10 +1,20 @@
 package com.bookmarkers.Data.Admin;
 
+import com.bookmarkers.Data.Item.Item;
+
+import java.util.List;
+
 /**
  * @Author : Yutong Jin
  * @date : 8/16/18
  * @Description :
  */
 public class Admin {
+    String Id;
+    String name;
+    String email;
+    String phone;
+    String username;
+    String password;
 
 }
